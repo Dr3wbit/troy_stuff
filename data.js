7d72146bbb1329203204340e224b9a2f3a3ce366
Andrew Harrington <eightbitandrew@gmail.com>
2020-09-28 20:24:49 -0700

    added animation

--- data.js
+++ data.js
@@ -2,4 +2,4 @@
 
 let list1 = ["brick & mortar shoppers", "app & web shoppers"]
 
-let list2 = ["poopoo.com", "peepee.com", "imgay.com"]
+let list2 = ["poopoo.com","thedumbzone.com", "peepee.com", "imgay.com", "Troyisstupid.org"]
--- main.js
+++ main.js
@@ -2,24 +2,59 @@
 
     let list1Ticker = 0
     let list2Ticker = 0
+    let previousItem1 = 0
+    let previousItem2 = 0
 
 
 $(document).ready(()=>{
-   const timer = setInterval(getNewText, 2000, list1, list2)
+    $('.variableText1').append('<div class="starterText">'+list1[0]+'</div>')
+    $('.variableText2').append('<div class="starterText">'+list2[0]+'</div>')
+    setTimeout(() => {
+        $('.variableText1').empty()
+        $('.variableText2').empty()
+    }, 3000);
+   const timer = setInterval(getNewText, 3000, list1, list2)
 });
 
 function getNewText(list1, list2){
+    
     if (list1.length <= list1Ticker) {
         list1Ticker = 0
     }
     if (list2.length <= list2Ticker) {
         list2Ticker = 0
     }
-    $('.variableText1').html(list1[list1Ticker])
-    $('.variableText2').html(list2[list2Ticker])
+
+    let list1Option = $('<div>', {
+        text: list1[list1Ticker],
+        class: "animatedText",
+        id: list1Ticker
+    })
+
+    let list2Option = $('<div>', {
+        text: list2[list2Ticker],
+        class: "animatedText",
+        id: list2Ticker
+    })
+    
+    removePreviousItem()
+
+    $('.variableText1').append(list1Option)
+    $('.variableText2').append(list2Option)
+
+    previousItem1 = list1Ticker
+    previousItem2 = list2Ticker
     list1Ticker++
     list2Ticker++
+
+
 }
     
     
+function removePreviousItem() {
 
+$('#' +previousItem1).remove()
+$('#' +previousItem2).remove()
+
+
+}
